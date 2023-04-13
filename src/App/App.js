0eb889d3c698f9ede3a0da9053e058ee8dfd6e5e
pavel khimich <pasha.khimich@mail.ru>
2023-04-13 20:19:11 +0300

    update

--- src/App/App.js
+++ src/App/App.js
@@ -1,18 +1,12 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Provider } from 'react-redux'
 
 import SideFilter from '../SideFilter/SideFilter'
 import MainContent from '../MainContent/MainContent'
 import store from '../store'
-import aviaSalesApi from '../aviaSalesApi/aviaSalesApi'
 import Logo from '../Logo/Logo'
 
-const aviaSalesService = new aviaSalesApi()
-
 const App = () => {
-  useEffect(() => {
-    aviaSalesService.getSearchId().then((res) => localStorage.setItem('searchId', res))
-  }, [])
   return (
     <Provider store={store}>
       <section className="avia-sales">
--- src/aviaSalesApi/aviaSalesApi.js
+++ src/aviaSalesApi/aviaSalesApi.js
@@ -1,16 +1,19 @@
 export default class aviaSalesApi {
-  searchId = localStorage.getItem('searchId')
   getSearchId = async () => {
     const request = await fetch('https://aviasales-test-api.kata.academy/search')
-    if (request.status === 500) {
-      return new Error('gone wrong')
-    }
     const result = await request.json()
+    if (!sessionStorage.getItem('searchId')) {
+      sessionStorage.setItem('searchId', result.searchId)
+    }
     return result.searchId
   }
   getTickets = async () => {
-    const request = await fetch(`https://aviasales-test-api.kata.academy/tickets?searchId=${this.searchId}`)
+    const searchId = !sessionStorage.getItem('searchId') ? await this.getSearchId() : sessionStorage.getItem('searchId')
+    const request = await fetch(`https://aviasales-test-api.kata.academy/tickets?searchId=${searchId}`)
     const result = await request.json()
+    if (result.stop) {
+      sessionStorage.removeItem('searchId')
+    }
     return result
   }
 }
